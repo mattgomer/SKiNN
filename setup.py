e17325efa476fb1ceb91cf8482e68eb3f85865fa
Matt Gomer <matthewrgomer@gmail.com>
2023-07-17 14:50:47 -0600

    added weights download to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 
+import os
+import urllib.request
+
 try:
-    from setuptools import setup, find_packages
+    from setuptools import setup, find_packages, find_namespace_packages
 except ImportError:
     from distutils.core import setup
+
+weights_url = "https://dox.uliege.be/index.php/s/mtphEyrQ5h4IRF8/download"
+weights_file = "SKiNN/weights/upsampling_cosmo_gen_norm_1channel_new.ckpt"
+weights_dir = os.path.dirname(weights_file)
+if not os.path.exists(weights_dir):
+    os.makedirs(weights_dir)
+
+if not os.path.exists(weights_file):
+    print('Downloading weights...')
+    urllib.request.urlretrieve(weights_url, weights_file)
     
 setup(name='SKiNN',
   version='1.1',
   description='Stellar Kinematics Neural Network',
   author='Luca Biggio',
   url='https://github.com/lucabig/lensing_odyssey_kinematics',
-  packages=find_packages(),
+  packages=find_packages()+find_namespace_packages(),
   include_package_data=True
       
  )
